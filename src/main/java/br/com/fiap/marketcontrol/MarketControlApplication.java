7ac61372f28e8ee2ac7da38f2a2028d43f479177
= <lucas.hbrd@gmail.com>
2023-03-09 17:06:36 -0300

    adicionando a primeira model

---
+++ src/main/java/br/com/fiap/marketcontrol/MarketControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.marketcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketControlApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/fiap/marketcontrol/models/Produto.java
@@ -0,0 +1,5 @@
+package br.com.fiap.marketcontrol.models;
+
+public class Produto {
+    
+}
